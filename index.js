f3e15b729ad1dc7e5d9582e6edebadef47b3d242
texturee <tonihurtado13@gmail.com>
2018-02-13 11:35:04 +0100

    image computer

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(process.env.PORT || 3000, function () {
-  console.log('Server running on 8080...');
+
+const port = process.env.PORT || 3000
+connect().use(serveStatic(__dirname)).listen(port, function () {
+  console.log(`Server running on ${port}`);
 });
 
 
